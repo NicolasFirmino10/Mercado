ff928b576f983f8e5f7c80ed0b8d1c59876a8fa3
Nicolas Firmino <nicolasneuma10@gmail.com>
2025-10-31 13:27:59 -0300

    deploy render

--- render.sh
+++ render.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 pip install --upgrade pip
 pip install -r requirements.txt --timeout 300 --retries 5
+pip install gunicorn
+python -c "from mercado import app, db; app.app_context().push(); db.create_all()"
