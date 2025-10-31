77771c9c2498b2e233eb7b58d0cc2c603ba7af17
Nicolas Firmino <nicolasneuma10@gmail.com>
2025-10-31 13:07:42 -0300

    deploy

---
+++ .vercel/cache/__init__/mercado/routes.py
@@ -0,0 +1,79 @@
+from mercado import app
+from flask import render_template, redirect, url_for, flash, request
+from mercado.models import Item, User
+from mercado.forms import CadastroForm, LoginForm, CompraProdutoForm, VendaProdutoForm
+from mercado import db
+from flask_login import login_user, logout_user, login_required, current_user
+
+@app.route('/')
+def page_home():
+    return render_template('home.html')
+
+@app.route('/produtos', methods=['GET', 'POST'])
+@login_required
+def page_produto():
+    compra_form = CompraProdutoForm()
+    venda_form = VendaProdutoForm()
+    if request.method == 'POST':
+        # Compra Produto
+        compra_produto = request.form.get('compra_produto')
+        produto_obj = Item.query.filter_by(nome=compra_produto).first()    
+        if produto_obj:
+            if current_user.compra_disponivel(produto_obj):
+                produto_obj.compra(current_user)
+                flash(f'Produto {produto_obj.nome} comprado com sucesso!', category = 'success') 
+            else:
+                flash(f'Você não tem dinheiro suficiente para comprar o produto {produto_obj.nome}!', category = 'danger')
+        
+        # Venda Produto 
+        venda_produto = request.form.get('venda_produto')
+        produto_obj_venda = Item.query.filter_by(nome=venda_produto).first()
+        if produto_obj_venda:
+            if current_user.venda_disponivel(produto_obj_venda):
+                produto_obj_venda.venda(current_user)
+                flash(f'Produto {produto_obj_venda.nome} vendido com sucesso!', category = 'success')
+            else:
+                flash(f'Algo deu errado com a venda do produto {produto_obj_venda.nome}', category = 'danger')
+        return redirect(url_for('page_produto'))
+
+    if request.method == 'GET':
+        itens = Item.query.filter_by(dono=None)
+        dono_itens = Item.query.filter_by(dono=current_user.id)
+        return render_template('produtos.html', itens=itens, compra_form=compra_form, dono_itens=dono_itens, venda_form=venda_form)
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def page_cadastro():
+    form = CadastroForm()
+    if form.validate_on_submit():
+        usuario = User(
+            usuario = form.usuario.data,
+            email = form.email.data,
+            senhacrip = form.senha1.data
+        )
+        db.session.add(usuario)
+        db.session.commit()
+        return redirect(url_for('page_produto'))
+    if form.errors != {}:
+        for err in form.errors.values():
+            flash(f'Erro de validação: {err}', category = 'danger')
+    return render_template('cadastro.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def page_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        usuario_logado = User.query.filter_by(usuario=form.usuario.data).first()
+        if usuario_logado and usuario_logado.converte_senha(senha_texto_claro = form.senha.data):
+            login_user(usuario_logado)
+            flash('Login feito com sucesso!', category = 'success')
+            return redirect(url_for('page_produto'))
+        else:
+            flash('Usuário ou senha incorretos. Tente novamente.', category = 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def page_logout():
+    logout_user()
+    flash('Logout feito com sucesso!', category = 'info')
+    return redirect(url_for('page_home'))
--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,14 +1,24 @@
+import os  
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 db = SQLAlchemy()
 login_manager = LoginManager()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
-app.config["SECRET_KEY"] = "1afb86cf05c9bcf0de4be1de"
+
+db_url = os.environ.get("DATABASE_URL")
+
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///mercado.db"
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "1afb86cf05c9bcf0de4be1de")
+
+
+
 db.init_app(app)
 bcrypt = Bcrypt(app)
 login_manager.init_app(app)
